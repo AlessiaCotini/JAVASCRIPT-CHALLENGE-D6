e455cb0768a2192289dfbb27eaefbc6a0abf38fd
AlessiaCotini <alessia.cotini@icloud.com>
2026-03-10 16:40:31 +0100

    Update D1.js

--- D1.js
+++ D1.js
@@ -21,55 +21,50 @@ REGOLE
 /* ESERCIZIO 2
  Crea una variable chiamata "myName" e assegna ad essa il tuo nome, sotto forma di stringa.
 */
-/*
+
 let myName = "Alessia";
-*/
 
 /* ESERCIZIO 3
  Scrivi il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.
 */
-/*
-x = 12;
+
+j = 12;
 y = 20;
-let somma = x + y;
+let somma = j + y;
 console.log(somma);
- */
 
 /* ESERCIZIO 4
  Crea una variable di nome "x" e assegna ad essa il numero 12.
 */
-/*
+
 let x = 12;
-*/
 
 /* ESERCIZIO 5
   Riassegna un nuovo valore alla variabile "myName" già esistente: il tuo cognome.
   Dimostra l'impossibilità di riassegnare un valore ad una variabile dichiarata con il costrutto const.
 */
-/*
+
 myName = "Cotini";
 const my2ndName = "Giada";
 my2ndName = "Gianmaria";
 console.log(my2ndName);
-*/
+
 /*=> in fase di verifica il valore di my2ndName rimarrà un errore*/
 /* ESERCIZIO 6
  Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12).
 */
-/*
+
 let y = 4;
 let risultato = x - y;
 console.log(risultato);
-*/
 
 /* ESERCIZIO 7
  Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
  Verifica che name1 sia diversa da name2 (suggerimento: è la stessa cosa di verificare che la loro uguaglianza sia falsa).
  EXTRA: verifica che la loro uguaglianza diventi true se entrambe vengono trasformate in lowercase (senza cambiare il valore di name2!).
 */
-/*
+
 let name1 = "jhon";
 let name2 = "Jhon";
 console.log(name1 === name2);
 console.log(name2.toLowerCase() === name1.toLowerCase());
-*/
